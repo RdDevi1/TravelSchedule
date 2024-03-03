e04d1217feb5d1f6aec66de11c61a210fe902256
Vitaly <anpilov.vitaly@gmail.com>
2024-03-03 14:56:38 +0300

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Vitaly Anpilov on 03.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
