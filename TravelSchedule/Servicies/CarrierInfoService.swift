a4674b3f47b7ac26031d5628082d39aee02d8107
Vitaly <anpilov.vitaly@gmail.com>
2024-03-10 20:50:48 +0300

    added CarrierInfoService

---
+++ TravelSchedule/Servicies/CarrierInfoService.swift
@@ -0,0 +1,35 @@
+//
+//  CarrierInfoService.swift
+//  TravelSchedule
+//
+//  Created by Vitaly Anpilov on 10.03.2024.
+//
+
+import OpenAPIRuntime
+import OpenAPIURLSession
+
+typealias Carrier = Components.Schemas.Carriers
+
+protocol CarrierInfoServiceProtocol {
+    func getCarrier(code: String) async throws -> Carrier
+}
+
+final class CarrierInfoService: CarrierInfoServiceProtocol {
+    private let client: Client
+    private let apikey: String
+    
+    init(client: Client, apikey: String) {
+        self.client = client
+        self.apikey = apikey
+    }
+    
+    func getCarrier(code: String) async throws -> Carrier {
+        let response = try await client.getCarrierInfo(query: .init(
+            apikey: apikey,
+            code: code)
+        )
+        
+        return try response.ok.body.json
+    }
+    
+}
