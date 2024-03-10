3f66ede1d223496f063c3f1cf2c31979853dcd79
Vitaly <anpilov.vitaly@gmail.com>
2024-03-10 18:03:28 +0300

    added NearestStationsService

---
+++ TravelSchedule/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  TravelSchedule
+//
+//  Created by Vitaly Anpilov on 10.03.2024.
+//
+
+import Foundation
+
+
+enum Constants {
+    
+    static let apiKey = "ed91adbc-d89d-4cdf-8125-53d19993775d"
+    
+}
