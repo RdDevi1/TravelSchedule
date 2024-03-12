9f2807fe711cac648c685535d97d8da8c669b159
Vitaly <anpilov.vitaly@gmail.com>
2024-03-12 16:57:11 +0300

    added functions for API testing

--- TravelSchedule/Helpers/Constants.swift
+++ TravelSchedule/Helpers/Constants.swift
@@ -13,3 +13,16 @@ enum Constants {
     static let apiKey = "ed91adbc-d89d-4cdf-8125-53d19993775d"
     
 }
+
+enum City {
+    static let tumen = "c55"
+    static let moscow = "c213"
+}
+
+enum Station {
+    static let svo = "s9600213"
+}
+
+enum ThreadUID {
+    static let thread = "4G-293_240416_c54_12"
+}
